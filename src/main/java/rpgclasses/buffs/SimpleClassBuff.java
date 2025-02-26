c332d583244187e539e9b2c2e38142f3f7d453d8
AizSave <aiz.save@gmail.com>
2025-02-26 17:46:37 +0100

    v1.0

---
+++ src/main/java/rpgclasses/buffs/SimpleClassBuff.java
@@ -0,0 +1,31 @@
+package rpgclasses.buffs;
+
+import necesse.engine.modifiers.Modifier;
+import necesse.engine.modifiers.ModifierLimiter;
+import necesse.engine.modifiers.ModifierValue;
+import necesse.entity.mobs.MobBeforeDamageOverTimeTakenEvent;
+import necesse.entity.mobs.buffs.ActiveBuff;
+import necesse.entity.mobs.buffs.BuffEventSubscriber;
+import necesse.entity.mobs.buffs.staticBuffs.Buff;
+import necesse.entity.mobs.buffs.staticBuffs.VicinityBuff;
+
+public class SimpleClassBuff extends Buff {
+    protected ModifierValue<?>[] modifiers;
+
+    public SimpleClassBuff(ModifierValue<?>... modifiers) {
+        this.canCancel = false;
+        this.isVisible = false;
+        this.isPassive = true;
+        this.shouldSave = false;
+        this.modifiers = modifiers;
+    }
+
+    public void init(ActiveBuff buff, BuffEventSubscriber eventSubscriber) {
+        for (ModifierValue<?> modifier : this.modifiers) {
+            modifier.apply(buff);
+        }
+    }
+
+    public void onLoad(ActiveBuff buff) {
+    }
+}
